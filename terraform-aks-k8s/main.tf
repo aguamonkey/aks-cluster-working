f73f16e136af974e65fbea5e7bef4377a1297ca5
Ubuntu <joshuabrowne@terraform-vm.vzz5ngmu3ekedanwv00wd2j03a.zx.internal.cloudapp.net>
2021-06-28 13:32:50 +0000

    Everything seems to be working

--- terraform-aks-k8s/main.tf
+++ terraform-aks-k8s/main.tf
@@ -25,14 +25,14 @@ resource "azurerm_resource_group" "vnet" {
 module "vnet" {
     source       = "./vnet"
     project_name = var.project_name
-    group_name   = azurerm_resource_group.k8s.name
+    group_name   = azurerm_resource_group.vnet.name
     location     = var.location
 }
 
 module "vm" {
     source        = "./vm"
     project_name  = var.project_name
-    group_name    = azurerm_resource_group.k8s.name
+    group_name    = azurerm_resource_group.vnet.name
     location      = var.location
     interface_ids = [module.vnet.interface_id]
 }
